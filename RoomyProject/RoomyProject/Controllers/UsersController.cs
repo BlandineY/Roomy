d594ceca9ab1922a445e83329873f9ef2d9f3720
Nico <nox_blue@hotmail.com>
2018-07-16 15:28:48 +0200

    Class + User controller + Index + validation avec messages de erreur  + morning

---
+++ RoomyProject/RoomyProject/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using RoomyProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoomyProject.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View();
+        }
+    }
+}
---
+++ RoomyProject/RoomyProject/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RoomyProject.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage ="Le champ nom est obligatoire")]
+        [Display(Name="Nom")]
+        public string Lastname { get; set; }
+
+        [Display(Name = "Prenom")]
+        public string Firstname { get; set; }
+
+        [Display(Name = "Mail")]
+        public string Mail { get; set; }
+
+        [Display(Name = "Birthdate")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "ConfirmedPassword")]
+        [DataType(DataType.Password)]
+        public string ConfirmedPassword { get; set; }
+    }
+}
